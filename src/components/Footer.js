c919352ff4b409005df8fd63a2513fac8ba6ce10
richard lontos <richard.lontos95@gmail.com>
2023-01-01 19:21:54 -0800

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
-import {  Col } from "react-bootstrap";
+// import {  Col } from "react-bootstrap";
 // import logo from "../assets/img/logo.svg";
 // import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+// import navIcon2 from "../assets/img/nav-icon2.svg";
+// import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
