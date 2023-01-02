01dace11b9c87f23e00b5e5f654d837b652ba612
richard lontos <richard.lontos95@gmail.com>
2023-01-01 18:51:04 -0800

    test

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { NavBar } from './components/NavBar';
-import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
-import { About } from './components/About';
-import { Projects } from './components/Projects';
-import {Skills} from './components/Skills';
-import { Banner } from './components/Banner';
+// import { Contact } from './components/Contact';
+// import { Footer } from './components/Footer';
+// import { About } from './components/About';
+// import { Projects } from './components/Projects';
+// import {Skills} from './components/Skills';
+// import { Banner } from './components/Banner';
 // import { Banner } from './components/Banner';
 
 function App() {
   return (
     <div className='App'>
       <NavBar /> 
-      <Banner />
+      {/* <Banner />
       <Skills />
       <Projects />
       <About />
       <Contact />
-      <Footer />
+      <Footer /> */}
       {/* <Banner /> */}
     </div>
   );
