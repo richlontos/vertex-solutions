0d56073c983f2068ce5b8b22f0d91c759fb00b87
richard lontos <richard.lontos95@gmail.com>
2023-01-02 17:24:14 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
 // import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {  Routes, Route } from 'react-router-dom';
-import { NavBar } from './components/NavBar';
-import Home from './pages/Home';
-import CarriersTab from './pages/CarriersTab';
-// import { Contact } from './components/Contact';
-import { Footer } from './components/Footer';
-// import { About } from './components/About';
-// import { Projects } from './components/Projects';
-// import {Skills} from './components/Skills';
-// import { Banner } from './components/Banner';
+import { Routes, Route } from 'react-router-dom';
 import Test from './pages/Test';
-import Faq from './pages/Faq';
-// import Services from './pages/Services';
-import WhatOffer from './pages/WhatOffer';
-import HomeServices from './pages/Services';
-import LTLServices from './pages/LTLServices';
-import RailServices from './pages/RailServices';
-import AirServices from './pages/AirServices';
-import OceanServices from './pages/OceanServices';
-import WarehouseServices from './pages/WarehouseServices';
-import VolumesServices from './pages/VolumeServices';
-import './components/serviceFolder/ServicesTest.css'
-// import { Banner } from './components/Banner';
+import CarriersTab from './pages/CarriersTab';
+// import HomeServices from './pages/HomeServices';
+// import LTLServices from './pages/HomeServices';
+// import RailServices from './pages/RailServices';
+// import AirServices from './pages/AirServices';
+// import OceanServices from './pages/OceanServices';
+// import VolumesServices from './pages/VolumeServices';
+// import WarehouseServices from './pages/WarehouseServices';
+// import WhatOffer from './pages/WhatOffer';
+// import Faq from './pages/Faq';
+import { NavBar } from './components/NavBar';
+import { Footer } from './components/Footer';
+import Home from './pages/Home';
+import Services from './pages/Services';
+
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path='carriers' element={<CarriersTab />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+{/* <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/test' element={<Test />}></Route>
         <Route path='/carriers' element={<CarriersTab />}></Route>
@@ -42,10 +50,4 @@ function App() {
         <Route path='/WarehouseServices' element={<WarehouseServices />}></Route>
         <Route path='/offer' element={<WhatOffer />}></Route>
         <Route path='/faq' element={<Faq />}></Route>
-      </Routes>
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
+      </Routes> */}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,12 +8,14 @@ import logo from './data/images/logo.png'
 // import navIcon3 from '../assets/img/nav-icon3.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
-    BrowserRouter as Router
+    Route,
+    Routes,
+    Link
 } from "react-router-dom";
 
 export const NavBar = () => {
 
-    // const [activeLink, setActiveLink] = useState('home');
+    const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
@@ -30,66 +32,66 @@ export const NavBar = () => {
         return () => window.removeEventListener("scroll", onScroll);
     }, [])
 
-    // const onUpdateActiveLink = (value) => {
-    //     setActiveLink(value);
-    // }
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
 
     return (
-        <Router>
-            <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img src={logo} alt="Logo" />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav">
-                        <span className="navbar-toggler-icon"></span>
-                    </Navbar.Toggle>
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ms-auto">
-                            <Nav.Link href="/" className={'active navbar-link'}>Home</Nav.Link>
-                            {/* <Nav.Link href="/services" className={'active navbar-link'}>Our Services</Nav.Link> */}
-                            <Nav.Link href="/carriers" className={'active navbar-link'}>Carriers</Nav.Link>
-                            <Dropdown class="drop-down closed">
-                                <Dropdown.Toggle variant="success" className="nav-button" id="dropdown-basic">
-                                    Company
-                                </Dropdown.Toggle>
+        <>
+<Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="/">
+          <img src={logo} alt="Logo" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
+            <span className="navbar-toggler-icon"></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/" className={'active navbar-link'}>Home</Nav.Link>
+              {/* <Nav.Link href="/services" className={'active navbar-link'}>Our Services</Nav.Link> */}
+              <Nav.Link href="/carriers" className={'active navbar-link'}>Carriers</Nav.Link>
+              <Dropdown class="drop-down closed">
+                <Dropdown.Toggle variant="success" className="nav-button" id="dropdown-basic">
+                  Company
+                </Dropdown.Toggle>
 
-                                <Dropdown.Menu id="dropMenu">
-                                    <Dropdown.Item id="itemColor" href="/resources">Resources</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/faq">FAQ</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/contactus">Contact Us</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/aboutus">About Us</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Dropdown class="drop-down closed">
-                                <Dropdown.Toggle variant="success" className="nav-button" id="dropdown-basic">
-                                    Our Services
-                                </Dropdown.Toggle>
+                <Dropdown.Menu id="dropMenu">
+                  <Dropdown.Item id="itemColor" href="/resources">Resources</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/faq">FAQ</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/contactus">Contact Us</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/aboutus">About Us</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Dropdown class="drop-down closed">
+                <Dropdown.Toggle variant="success" className="nav-button" id="dropdown-basic">
+                  Our Services
+                </Dropdown.Toggle>
 
-                                <Dropdown.Menu id="dropMenu">
-                                    <Dropdown.Item id="itemColor" href="/services">FTL</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/servicesLTL">LTL</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/RailServices">Rail</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/AirServices">Air</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/OceanServices">Ocean</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/volumePartial">Volume</Dropdown.Item>
-                                    <Dropdown.Item id="itemColor" href="/WarehouseServices">Warehousing and Distribution</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                            <Nav.Link href="https://vertexsolutions.taicloud.net/Main/Home" className="navbar-link">Track shipment/Login</Nav.Link>
-                        </Nav>
-                        <span className="navbar-text">
-                            {/* <div className="social-icon">
+                <Dropdown.Menu id="dropMenu">
+                  <Dropdown.Item id="itemColor" href="/services">FTL</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/servicesLTL">LTL</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/RailServices">Rail</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/AirServices">Air</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/OceanServices">Ocean</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/volumePartial">Volume</Dropdown.Item>
+                  <Dropdown.Item id="itemColor" href="/WarehouseServices">Warehousing and Distribution</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+              <Nav.Link href="https://vertexsolutions.taicloud.net/Main/Home" className="navbar-link">Track shipment/Login</Nav.Link>
+            </Nav>
+            <span className="navbar-text">
+              {/* <div className="social-icon">
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div> */}
-                            <HashLink to='#connect'>
-                                <button className="vvd"><span>Request a Qoute</span></button>
-                            </HashLink>
-                        </span>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </Router>
+              <HashLink to='/test'>
+                <button className="vvd"><span>Request a Qoute</span></button>
+              </HashLink>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      </>
     )
 }
